7a71418d6756ea7e76021ce60b4f3e183f066219
Luc Bloom <luc@storygiantgames.com>
2025-11-29 21:25:08 +0100

    New interface (python)

---
+++ package_install.bat
@@ -0,0 +1,4 @@
+call ares-package app
+REM mv com.lucbloom.slideshow_1.0.0_all.ipk app.ipk
+call ares-install --device lg com.lucbloom.slideshow_1.0.0_all.ipk
+call "D:\Programs\webOS Dev Manager\webos-dev-manager.exe" install -d lg -f com.lucbloom.slideshow_1.0.0_all.ipk
